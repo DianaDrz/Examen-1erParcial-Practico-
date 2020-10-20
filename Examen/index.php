996d9b7a6a9e053cbdf7d222bd053a0f262a5cc9
Druizs <diana.ciberdrz@gmail.com>
2020-10-19 20:01:43 -0400

    Pregunta 1 - Completo

--- Examen/index.php
+++ Examen/index.php
@@ -1,15 +1,26 @@
 <html>
 <head>
   <title>login</title>
+  <link href="estilos.css" rel="stylesheet" />
 </head>
-<body>
-  <div >
-    <h2>Inicio de sesión</h2>
-    <form action="autentificacion.php" method="POST">
-      <label>Usuario</label><input type="text" name="ci" ><br><br>
-      <label >Contraseña</label><input type="password" name="clave" ><br><br>
-      <input type="submit" name="aceptar" value="Iniciar Sesion">
-    </form>
-  </div>
+<body id="inicio">
+	<div class="d0">
+        <div class="d1">
+            <div class="d2">
+                <h1 class="t1">UMSA - PRIMER EXAMEN PARCIAL INF -324</h1>
+                <div class="d3">
+                    <img class="fotog" src="imagenes/usuarioGlobal.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+	<div id="a1">
+	    <form action="autentificacion.php" method="POST">
+	    	<h2>INICIAR SECIÓN</h2>
+	      <label>Usuario</label><input type="text" placeholder="Ingrese su CI" name="ci" ><br><br>
+	      <label >Contraseña</label><input type="password" placeholder="Ingrese su contraseña" name="clave" ><br><br>
+	      <input type="submit" name="aceptar" value="Iniciar Sesión">
+	    </form>
+	</div>
 </body>
 </html>
--- Examen/login.php
+++ Examen/login.php
@@ -1,8 +1,18 @@
-
+<?php
+    include "conexion.inc.php";
+    session_start();
+    if(!isset($_SESSION['ci'])){
+        header("Location: index.php"); 
+    }
+    $ci=$_SESSION['ci'];
+    $sql=mysqli_query($conn,"SELECT * FROM identificador WHERE ci='$ci'");
+    $fila=mysqli_fetch_row($sql);
+    $nombre=$fila[1];
+?>
 <html>
 <head>
 	<title>BIENVENIDO USUARIO</title>
-    <link href="style.css" rel="stylesheet" />
+    <link href="estilos.css" rel="stylesheet" />
     <style >
     .block{
         display: none;
@@ -13,7 +23,7 @@
             var cuerpo=document.getElementById("cuerpo");
             cuerpo.style.backgroundColor=z.value;
         }
-        function myFunction(ci){
+        function perfil(ci){
             console.log("1")
             var x = document.getElementById(ci);
             console.log("2")
@@ -25,29 +35,32 @@
 </head>
 
 <body id="cuerpo">
-
-    <div id="74347" class="block" ><img src="imagen1.jpg" width="300" height="150"></div>
-    <div id="84348" class="block" ><img src="imagen2.jpg" width="300" height="150"></div>
-    <div id="94449" class="block" ><img src="imagen3.jpg" width="300" height="150"></div>
-
-	<h2>Seleccione un color</h2>
-	<select name="color" id="color" onchange="fondo(this)">  
-        <option value="red">Rojo</option>
-        <option value="yellow">Amarillo</option>
-        <option value="blue">Azul</option>
-	</select>	
+    <div class="d0">
+        <div class="d1">
+            <div class="d22">
+                <h1 class="t1">INF 324 - Pregunta 1</h1>
+                <div class="d3">
+                    <?php echo '<p class="t2">BIENVENID@: '.$nombre.'</p>' ;
+                    ?>
+                    <div id="74347" class="block" ><img src="imagenes/p1.jpg" width="100px" height="100px"></div>
+                    <div id="84348" class="block" ><img src="imagenes/p2.jpg" width="100px" height="100px"></div>
+                    <div id="84645" class="block" ><img src="imagenes/p3.jpg" width="100px" height="100px"></div>
+                    <div id="94449" class="block" ><img src="imagenes/p4.jpg" width="100px" height="100px"></div>
+                    <?php echo '<script>perfil('.strval($ci).')</script> ';?>
+                    <p class="content-text"><a href="cerrar.php">CerrarSession</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sele">
+        <h2>Seleccione un color para cambiar el fondo de pantalla</h2>
+        <select  id="color" class="sel" onchange="fondo(this)">
+            <option value="#4B4D4C">Color por defecto</option>  
+            <option value="red">Rojo</option>
+            <option value="yellow">Amarillo</option>
+            <option value="blue">Azul</option>
+            <option value="pink">Rosado</option>
+        </select>
+    </div>
 </body>
 </html>
-<?php
-    include "conexion.inc.php";
-    session_start();
-    if(!isset($_SESSION['ci'])){
-        header("Location: index.php"); 
-    }
-    $ci=$_SESSION['ci'];
-    $sql=mysqli_query($conn,"SELECT * FROM identificador WHERE ci='$ci'");
-    $fila=mysqli_fetch_row($sql);
-    $nombre=$fila[1];
-    echo "BIENVENIDO USUARIO: ".$nombre;
-    echo '<script>myFunction('.strval($ci).')</script> ';
-?>
